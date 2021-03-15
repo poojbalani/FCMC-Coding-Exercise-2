a00ab1b38f6580452ea3d7b3f7ade43b37245a86
pbalani <pbalani@umich.edu>
2021-03-15 15:35:51 -0400

    Initial Commit

---
+++ FMCCcodingexercise/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FMCCcodingexercise
+//
+//  Created by Pooja Balani on 3/15/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
